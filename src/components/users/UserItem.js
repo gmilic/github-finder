1a6c767c5170ca5f127a40fd1c30e8426a219bd5
gmilic <gmilic@gmail.com>
2019-12-09 22:50:30 +0100

    lesson-023

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const UserItem = ({user: { login, avatar_url, html_url }}) => {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 
-    return (
-      <div className="card text-center">
-        <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn dark btn-sm my-1">More</a>
-        </div>
+  return (
+    <div className="card text-center">
+      <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`/user/${login}`} className="btn dark btn-sm my-1">More</Link>
       </div>
-    )
+    </div>
+  )
 
 }
 
